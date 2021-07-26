98ac345c65957eca2d92375f6aee7481174a75e0
opusvar <sprojdev@gmail.com>
2021-07-25 22:33:55 -0400

    Fixed typos, updated plans

--- main.go
+++ main.go
@@ -15,21 +15,25 @@ func main() {
 	if eludedGuards >= 50 {
 		f.Println("Looks like you've managed to make it past the guards.  Good job, but remember, this is the first step.")
 		f.Println("The heist continues.")
+		f.Println("Still got that safe to crack... \n")
 	} else {
 		isHeistOn = false
 		f.Println("Failed to elude the guards.  The heist fails. ")
 		f.Println("Plan a better disguse next time?")
 	}
 
-    openedVault := m.Intn(100) // determins if the robbers open the vault
+    openedVault := m.Intn(100) // determines if the robbers open the vault
 
-    if openedVault >= 70 {
+    if isHeistOn && openedVault >= 70 {
         f.Println("Vault Open, Grab and GO!")
+		f.Println("The heist continues.")
+		f.Println("Still got make the escape.. \n")
 	} else if isHeistOn && openedVault < 70 {
         isHeistOn = false
 		f.Println("Safecracker failed to open the vault.  You missed your shot!")
 		f.Println("The boss doesn't tolerate failure, better go on vacation.")
-	}
+    
+	
 
 	f.Println("Heist Status:", isHeistOn)
 
