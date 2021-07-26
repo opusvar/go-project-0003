774fc7f9d1a177ce421e7adaa26421fc3b2fa84d
opusvar <sprojdev@gmail.com>
2021-07-25 22:20:01 -0400

    First checkpoint complete

--- main.go
+++ main.go
@@ -7,5 +7,24 @@ import (
 )
 
 func main() {
+    m.Seed(t.Now().UnixNano())
+	isHeistOn := true
+    eludedGuards := m.Intn(100)
+
+	if eludedGuards >= 50 {
+		f.Println("Looks like you've managed to make it past the guards.  Good job, but remember, this is the first step.")
+		f.Println("The heist continues.")
+	} else {
+		isHeistOn = false
+		f.Println("Failed to elude the guards.  The heist fails. ")
+		f.Println("Plan a better disguse next time?")
+	}
+
+	f.Println("Heist Status:", isHeistOn)
+
+
+
+
+
 
 }
