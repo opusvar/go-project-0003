0521b68d114760b38de9ab5a0fdc4e75b9e442e7
opusvar <sprojdev@gmail.com>
2021-07-25 22:28:24 -0400

    Second checkpoint, passed

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main() {
-    m.Seed(t.Now().UnixNano())
-	isHeistOn := true
-    eludedGuards := m.Intn(100)
+    m.Seed(t.Now().UnixNano()) // determines the seed
+	isHeistOn := true  // declares heist status
+    eludedGuards := m.Intn(100) // determines if the robbers eludes the guards
 
+	// logic to detmine if the robbers elude the guards
 	if eludedGuards >= 50 {
 		f.Println("Looks like you've managed to make it past the guards.  Good job, but remember, this is the first step.")
 		f.Println("The heist continues.")
@@ -20,8 +21,17 @@ func main() {
 		f.Println("Plan a better disguse next time?")
 	}
 
-	f.Println("Heist Status:", isHeistOn)
+    openedVault := m.Intn(100) // determins if the robbers open the vault
 
+    if openedVault >= 70 {
+        f.Println("Vault Open, Grab and GO!")
+	} else if isHeistOn && openedVault < 70 {
+        isHeistOn = false
+		f.Println("Safecracker failed to open the vault.  You missed your shot!")
+		f.Println("The boss doesn't tolerate failure, better go on vacation.")
+	}
+
+	f.Println("Heist Status:", isHeistOn)
 
 
 
